d1b8150410056c4060f8415f0f209b7c91987479
Steve Richey <srichey@floatlearning.com>
2015-05-12 14:19:35 -0500

    append .sh for clarity

--- haxelibify.sh
+++ haxelibify.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit script on error
+set -e
+
 # create haxelib archive
 # (don't include copyrighted files)
 zip -vr comscore.zip ./* -x "*.DS_Store" "*.a" "/native/iphone/include/*.h" "*.o"
