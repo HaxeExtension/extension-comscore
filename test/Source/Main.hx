7d93e2515759fd3fabe1755b11b6fa1f035ff76c
Steve Richey <srichey@floatlearning.com>
2015-05-12 12:15:09 -0500

    add test project

---
+++ test/Source/Main.hx
@@ -0,0 +1,22 @@
+package;
+
+import openfl.display.Sprite;
+import extension.comscore;
+
+class Main extends Sprite
+{
+	private static inline EXAMPLE_VALUE:String = "EXAMPLE_VALUE";
+
+	public function new()
+	{
+		super();
+		
+		// this test just calls everything in the SDK.
+		ComScore.setCustomerC2(EXAMPLE_VALUE);
+		ComScore.setPublisherSecret(EXAMPLE_VALUE);
+		ComScore.setAppContext();
+		ComScore.onUxActive();
+		ComScore.onUxInactive();
+		ComScore.enableAutoUpdate();
+	}
+}
